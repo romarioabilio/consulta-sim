ca0ddff2b59277c4ff423fb9e0b44ef1cc7da25c
Romário Abílio <romarioabilio22082001@gmail.com>
2023-10-27 15:21:51 -0300

    Removendo acesso direto a view Endereço

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :enderecos
   resources :consulta
   resources :medicos
-  resources :pacientes
+  resources :pacientes do
+    resources :enderecos
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
