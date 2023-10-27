827e3a8bc3ccb649600e8a07558e4aecb5b23582
Romário Abílio <romarioabilio22082001@gmail.com>
2023-10-27 02:18:33 -0300

    Implementando Validations para Consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -1,4 +1,16 @@
 class Consultum < ApplicationRecord
   belongs_to :paciente
   belongs_to :medico
+
+  validates :data, :horario, presence: true
+  validate :data_da_consulta_validar
+  validates_associated :medico
+  validates_associated :paciente
+
+  def data_da_consulta_validar
+    if data.present? && data < Date.today
+      errors.add(:data_nascimento,"é inválida !")
+    end
+  end
+
 end
