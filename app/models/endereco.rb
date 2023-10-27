a87101653ed6fa48d574c94d14fdae4696680e88
Romário Abílio <romarioabilio22082001@gmail.com>
2023-10-27 02:06:56 -0300

    Implementando Validations para Endereço

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,10 @@
 class Endereco < ApplicationRecord
   belongs_to :paciente
+
+  validates :cep, :cidade, :bairro, :logradouro, :complemento, presence: true
+  validates :cep, length: { is: 8 ,message:"inválido"},numericality: { only_integer: true }
+  validates :cidade, length: { in: 3..20 }, format: { with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/}
+  validates :bairro, length: {in: 3..20}, format: { with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/}
+  validates :logradouro, length: {in:3..50}, format: { with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/}
+  validates :complemento, length: {in: 3..30}, format: { with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/}
 end
