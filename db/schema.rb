465606f23875346671c391a468df1e65b35193c9
Romário Abílio <romarioabilio22082001@gmail.com>
2023-10-27 00:01:20 -0300

    Gerando Scaffold Médicos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_27_024617) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_27_025821) do
+  create_table "medicos", force: :cascade do |t|
+    t.string "nome_completo"
+    t.string "cpf"
+    t.string "email"
+    t.string "especialidade"
+    t.string "crm"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pacientes", force: :cascade do |t|
     t.string "nome_completo"
     t.date "data_nascimento"
