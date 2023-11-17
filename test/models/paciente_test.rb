769aecbb9bac32be5a2fd04814a9e242562fd5eb
Romário Abílio <romarioabilio22082001@gmail.com>
2023-11-17 01:04:03 -0300

    Testes de model: paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -4,4 +4,16 @@ class PacienteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test 'Cadastrar paciente com dados validos' do
+    paciente = Paciente.new nome_completo: 'Romário Abílio', data_nascimento: "2001-08-22",
+                            cpf: "11403528498", email: "romarioabilio@hotmail.com"
+    assert paciente.save
+  end
+  test 'Cadastrar paciente com CPF vazio' do
+    paciente = Paciente.new nome_completo: 'Romário Abílio', data_nascimento: "2001-08-22",
+                            cpf: "", email: "romarioabilio@hotmail.com"
+    assert_not paciente.save
+  end
 end
